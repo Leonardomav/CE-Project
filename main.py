0dd684ddabee6ddc4968de543ad1e16f5e30edd0
Leonardomav <leomavieira97@gmail.com>
2019-05-07 16:03:44 +0100

    Added Berlin 52 Solution and Function to read solution

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import antcolony as aco
 from tsp import le_coordenadas_tsp, dicio_cidades, distancia
 
+def read_solution():
+    with open('test_cases/berlin52.opt.tour') as fp:
+        solution = fp.readlines()
+        solution = list(map(int,solution[4:len(solution)-1]))
+        return solution
+
 # load world
 SmallWorld = le_coordenadas_tsp('test_cases/berlin52.tsp')
-BigWorld = le_coordenadas_tsp('test_cases/world.tsp')
+SmallWorld_sol = read_solution()
 dicio = dicio_cidades(SmallWorld)
 
 # Preforms ACO
