c6d3880209fc79a094465414a8046a5d51d26916
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2019-05-20 12:18:55 +0100

    master: fix aco metrics.

--- utils/csv_converter.py
+++ utils/csv_converter.py
@@ -30,4 +30,19 @@ class CsvConverter():
 
     def aco_to_csv(self):
         ''' Convert ACO data to CSV file '''
-        pass
+        csv = open(self.filename, "w")
+
+        population_initialization_title_row = "Population Initialization, Population Size, Iterations, Alpha, Beta, Evaporation Coefficient\n"
+        csv.write(population_initialization_title_row)
+        csv.write(str(self.data["population_initialization_time"])+ "," + str(self.data["population_size"]) + "," + str(self.data["iterations"]) + "," + str(self.data["alpha"]) + "," + str(self.data["beta"]) + "," + str(self.data["evaporation_coeficient"]) + "\n")
+
+        data_title_row = "Iteration, Best Individual (Position), Best Individual (Fitness), Fitness Average, Calculation Time\n"
+        csv.write(data_title_row)
+
+        generation = 0
+        for best_individual, fitness_average, calculation_time in zip(self.data["best_individuals"], self.data["fitness_averages"], self.data["generation_calculation_times"]):
+            row = str(generation) + ",\"" + str(best_individual[0]) + "\",\"" + str(best_individual[1]) + "\"," + str(fitness_average) + "," + str(calculation_time) + "\n"
+            csv.write(row)
+            generation+=1
+
+        csv.close()
